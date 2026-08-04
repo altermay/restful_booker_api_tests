9fff7e6fddc24e152b469354631886f6bb57466a
altermay <163026657+altermay@users.noreply.github.com>
2026-08-04 13:19:42 -0500

    commit changes

--- tests/test_get_booking_ids_by_name.py
+++ tests/test_get_booking_ids_by_name.py
@@ -3,7 +3,7 @@ import requests
 base_url = "https://restful-booker.herokuapp.com"
 
 def test_get_booking_ids_by_name():
-    response = requests.get(f"{base_url}/booking?firstname=firstname4&lastname=lastname4")
+    response = requests.get(f"{base_url}/booking?firstname=Mary&lastname=Wilson")
     assert response.status_code == 200
     body = response.json()
     assert len(body) > 0
